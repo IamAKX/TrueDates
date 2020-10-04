0544f5ba068809b4f7ca3d649cd43e6aed0cd1ba
Akash Giri <akashgiri27aug@gmail.com>
2020-10-04 12:12:15 +0530

    Fixed location bug in EditProfile

--- app/src/main/java/com/neosao/truedates/screens/EditProfile.java
+++ app/src/main/java/com/neosao/truedates/screens/EditProfile.java
@@ -635,6 +635,8 @@ public class EditProfile extends AppCompatActivity implements View.OnClickListen
                                 user.getMembersettings().get(0).setLatitude(String.valueOf(loc.getLatitude()));
                                 user.getMembersettings().get(0).setLongitude(String.valueOf(loc.getLongitude()));
                                 List<Address> addresses = geocoder.getFromLocation(loc.getLatitude(), loc.getLongitude(), 1);
+                                if(null == addresses || addresses.isEmpty())
+                                    return;
                                 Address address = addresses.get(0);
                                 user.getMembersettings().get(0).setCurrentLocation(address.getLocality() + ", " + address.getCountryName());
                                 location.setText(address.getLocality() + ", " + address.getCountryName());
